c447b00374965a545dba9aaec1c30d80df77bd8e
nicolasdca <nicolasdavid193@gmail.com>
2023-11-14 21:59:52 -0300

    NodeJs 1 Alura - Aula 2

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import { default as chalk } from 'chalk';
+import { error } from 'console';
+import fs from 'fs';
 
-console.log(
-  chalk.blue(
-    'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e solt'
-  )
-);
-console.log(
-  'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e solt'
-);
-console.log(
-  'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e solt'
-);
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(texto);
+  } catch (erro) {
+    trataErro(erro);
+  }
+}
+
+// promises com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.promises.readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => {
+//         console.log(chalk.green(texto))
+//     })
+//     .catch(trataErro)
+// }
+
+pegaArquivo('./arquivos/texto.md');

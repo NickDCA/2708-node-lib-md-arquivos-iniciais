759b506264f7fcd9b0041b9bdcdb2fa5a55d5dce
nicolasdca <nicolasdavid193@gmail.com>
2023-11-15 16:15:48 -0300

    NodeJs 1 Alura - Aula 4.1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function extraiLinks(texto) {
   const resultados = capturas.map((captura) => ({
     [captura[1]]: captura[2],
   }));
-  return resultados;
+  return resultados.length !== 0 ? resultados : 'não há links no arquivo';
 }
 
 function trataErro(erro) {
@@ -20,13 +20,13 @@ async function pegaArquivo(caminhoDoArquivo) {
   try {
     const encoding = 'utf-8';
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(extraiLinks(texto));
+    return extraiLinks(texto);
   } catch (erro) {
     trataErro(erro);
   }
 }
 
-pegaArquivo('./arquivos/texto.md');
+export default pegaArquivo;
 
 // \[[^[\]]*?\]
 // \(https?:\/\/[^\s?\#\.].[^\s]*\)
